a410f10fe12cab2ec9ab622f70d7e1114c6e3b36
CarmineCaiazzo <KarmineKaiazzo11@gmail.com>
2019-01-29 12:37:35 +0000

    Aggiunto errore 404

--- index.js
+++ index.js
@@ -6,4 +6,9 @@ app.get('/', function (req, res) {
   res.end('hello world');
 });
 
+app.use( function (req, res)  {
+  res.status(404);
+  res.end("Page not Found");
+});
+
 app.listen(process.env.PORT || 3000);
